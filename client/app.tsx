54083ea58732de1f3e9a6c3ca6802cf0b7beba43
Yehor Luzianin <yehor.luzianin@synapse.com>
2019-01-29 18:30:57 +0200

    config

---
+++ client/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react' // eslint-disable-line
+import { render } from 'react-dom';
+import routes from './routes/';
+
+// global._babelPolyfill = false;
+
+render(routes, document.getElementById('root'));
